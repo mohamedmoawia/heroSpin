49c7aa7b2b7864c85bf77b5d33e99c4d7f45919d
MohamedMoawya <mo3wia94@gmail.com>
2022-10-08 14:05:13 +0200

    feat: add 404 page

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+import styles from '../styles/Home.module.scss';
+
+const PageNotFound = () =>{
+    return <div className={styles.notFound}>
+        <img src="/page-not-found.png"/>
+    </div>
+}
+
+export default PageNotFound
