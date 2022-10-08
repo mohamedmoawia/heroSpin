b3ac14955ff2ce635916cf9cde4ebfaf53859976
MohamedMoawya <mo3wia94@gmail.com>
2022-10-08 14:07:12 +0200

    static: add static data and images

---
+++ resources/static.js
@@ -0,0 +1,14 @@
+export const Heros = [
+  { name: 'Batman', img: '/batman.jpg', color: 'black', info: 'Batman is one of the most iconic superheroes ever', id: 1 , query: "batman"},
+  { name: 'Hulk', img: '/hulk.png', color: '#173f17', info: 'The Incredible Hulk embodies the darkest, and angriest super hero.', id: 2 , query: "hulk" },
+  { name: 'Spider man', img: '/spider-man.png', color: '#4d1208', info: "Spider-Man wasn't actually going to be a spider-based superhero", id: 3 , query: "deadpool" },
+  {
+    name: 'Super man',
+    img: '/super-man.png',
+    color: '#323e56',
+    info: 'Superman has incredible strength, x-ray vision, heat vision, super speed, and the ability to fly.',
+    id: 4,
+     query: "superMan"
+  }
+];
+export const APIkey = 'd607b1e9';
