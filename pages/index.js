56ef95e3f5f3a55094216b037b9efabff946cf69
MohamedMoawya <mo3wia94@gmail.com>
2022-10-10 00:26:55 +0200

    refactor: add images folder to public folders

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import Head from 'next/head';
-import Image from 'next/image';
-
 import SuperHero from '../components/superHero';
 import styles from '../styles/Home.module.scss';
 import { Heros } from '../resources/static';
@@ -10,27 +8,28 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Hero Spin</title>
-        <meta name="description" content="Chooose " />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Choose Your hero and will pick you a movie" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
 
       <main>
         <section className={styles.hero}>
           <div className={styles.hero__imgContainer}>
-            <img src="/home-hero-img.jpg" className={styles.hero__img} />
+            <img src="images/home-hero-img.jpg" className={styles.hero__img} />
             <div className={styles.hero__overlay}>
               <h1>HERO SPIN</h1>
               <p>Choose Your Hero Wisely</p>
             </div>
           </div>
           <div className="indicatorArrow">
-            <img className="arrow-img" src="/down-arrow.png" />
+            <img className="arrow-img" src="images/down-arrow.png" />
           </div>
         </section>
         {Heros.map((hero, index) => {
           return <SuperHero key={hero.id} name={hero.name} info={hero.info} img={hero.img} color={hero.color} index={index} query={hero.query} />;
         })}
       </main>
+      
     </div>
   );
 }
