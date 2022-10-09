e3b75d5482c8db493b68a7fa11bfc89f819e210c
MohamedMoawya <mo3wia94@gmail.com>
2022-10-10 00:45:50 +0200

    refactor: add Head section to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Hero Spin</title>
+        <meta name="description" content="Choose Your hero and will pick you a movie" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,11 @@
-import Head from 'next/head';
 import SuperHero from '../components/superHero';
 import styles from '../styles/Home.module.scss';
 import { Heros } from '../resources/static';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Hero Spin</title>
-        <meta name="description" content="Choose Your hero and will pick you a movie" />
-        <link rel="icon" href="/favicon.png" />
-      </Head>
-
-      <main>
+    <main className={styles.container}>
+      <div>
         <section className={styles.hero}>
           <div className={styles.hero__imgContainer}>
             <img src="images/home-hero-img.jpg" className={styles.hero__img} />
@@ -28,8 +21,8 @@ export default function Home() {
         {Heros.map((hero, index) => {
           return <SuperHero key={hero.id} name={hero.name} info={hero.info} img={hero.img} color={hero.color} index={index} query={hero.query} />;
         })}
-      </main>
+      </div>
       
-    </div>
+    </main>
   );
 }
